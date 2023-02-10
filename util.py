c468da6495d2ab7ac5e31692804d3d16c4d7fc3c
whb605 <122977955+whb605@users.noreply.github.com>
2023-02-10 09:14:06 +0800

    Update util.py

--- util.py
+++ util.py
@@ -1,22 +1,5 @@
-import community
-import matplotlib.pyplot as plt
-import networkx as nx
-import numpy as np
-
 def node_iter(G):
-    # if float(nx.__version__)<2.0:
-    #     return G.nodes()
-    # else:
-    #     return G.nodes
     return G.nodes
 
 def node_dict(G):
-    # if float(nx.__version__)>2.1:
-    #     node_dict = G.nodes
-    # else:
-    #     node_dict = G.node
-    # return node_dict
     return G.nodes
-
-
-
